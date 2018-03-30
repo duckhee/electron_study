7f592a0a1afc2d2ba6b2efd166b40bc78a58943e
duckheewon <fain9301@yahoo.com>
2018-03-30 11:15:23 +0900

    testing port

--- plugins/radon/controller.js
+++ plugins/radon/controller.js
@@ -1,5 +1,29 @@
-function RadonValue($scope, $http, $interval) {
+var SerialPort = require('serialport');
 
+var parsers = SerialPort.parsers;
+var parser = new parsers.Readline({
+    delimiter: '\r\n',
+
+});
+
+var port = new SerialPort('/dev/ttypACM0', {
+    baudrate: 9600
+});
+
+port.pipe(parser);
+
+port.on('open', function() {
+    console.log('testing open !');
+});
+
+port.on('error', function(err) {
+    console.log('error ::::::::: ', err);
+});
+
+function RadonValue($scope, $http, $interval) {
+    port.on('data', function(data) {
+        console.log('get data :::::: ', data);
+    })
 
 
 
