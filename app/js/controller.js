afe4d5e7615e8c885a2edaef9e6e8644644ce8e0
duckheewon <fain9301@yahoo.com>
2018-03-30 11:46:00 +0900

    testing

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,3 +1,23 @@
+var SerialPort = require('serialport');
+var parsers = SerialPort.parsers;
+var parser = new parsers.Readline({
+    delimiter: '\r\n'
+});
+
+var port = new SerialPort('/dev/ttyACM0', {
+    baudrate: 9600
+});
+
+port.pipe(parser);
+
+port.on('open', function() {
+    console.log('testing open');
+})
+
+port.on('error', function(err) {
+    console.log('open error ::::::::: ', err);
+})
+
 (function(angular) {
     'use strict';
 
@@ -10,13 +30,19 @@
             $scope.date = new moment();
         }
         //get radon value
-
+        function get_radon() {
+            port.on('data', function(data) {
+                $scope.radon = data;
+                console.log('get data :::::', data);
+            });
+        }
         //get door value
 
         //get fan status
 
         _this.init = function() {
             $interval(updateTime, 1000);
+            $interval(get_radon, 1000);
             var defaultView = function() {
                 console.debug("Ok, going to default view...");
 
--- plugins/radon/controller.js
+++ plugins/radon/controller.js
@@ -6,33 +6,33 @@ var parser = new parsers.Readline({
 
 });
 
-var port = new SerialPort('/dev/ttypACM0', {
-    baudrate: 9600
-});
+// var port = new SerialPort('/dev/ttypACM0', {
+//     baudrate: 9600
+// });
 
-port.pipe(parser);
+// port.pipe(parser);
 
-port.on('open', function() {
-    console.log('testing open !');
-});
+// port.on('open', function() {
+//     console.log('testing open !');
+// });
 
-port.on('error', function(err) {
-    console.log('error ::::::::: ', err);
-});
+// port.on('error', function(err) {
+//     console.log('error ::::::::: ', err);
+// });
 
-function RadonValue($scope, $http, $interval) {
+// function RadonValue($scope, $http, $interval) {
 
-    var get_value = function() {
-        port.on('data', function(data) {
-            console.log('get data :::::: ', data);
+//     var get_value = function() {
+//         port.on('data', function(data) {
+//             console.log('get data :::::: ', data);
 
-        });
+//         });
 
-    }
+//     }
 
-    $scope.get_value = get_value;
+//     $scope.get_value = get_value;
 
 
 
-    angular.module('radonapp').controller('RadonValue', RadonValue);
-}
+//     angular.module('radonapp').controller('RadonValue', RadonValue);
+// }
